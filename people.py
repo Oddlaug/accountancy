e34eab9d1507f9780add42455be266d8637b88a1
Oddlaug <47041203+Oddlaug@users.noreply.github.com>
2019-03-04 08:26:13 +0300

    Add files via upload

---
+++ people.py
@@ -0,0 +1,9 @@
+# -*- coding:utf-8 -*-
+
+
+def get_employees():
+    print(f'Вызвана функция: {get_employees.__name__}')
+
+
+if __name__ == '__main__':
+    get_employees()
---
+++ salary.py
@@ -0,0 +1,6 @@
+def calculate_salary():
+    print(f'Вызвана функция: {calculate_salary.__name__}')
+
+
+if __name__ == '__main__':
+    calculate_salary()
